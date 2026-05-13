c5c5495d43780915ee27373ff493f426cde81a70
Gwainne <matias.acosta.mlodziejewski@gmail.com>
2026-05-13 19:58:40 -0300

    Proyecto base Spring boot para el TP Steam

---
+++ src/main/java/com/steamunla/steamunla/SteamunlaApplication.java
@@ -0,0 +1,13 @@
+package com.steamunla.steamunla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamunlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamunlaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steamunla/steamunla/SteamunlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steamunla.steamunla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamunlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
